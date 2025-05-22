03a2db3510f0ce5b52959979f95a19444d1af32f
chaudhary-mukesh <153888197+chaudhary-mukesh@users.noreply.github.com>
2025-05-22 18:30:09 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,20 @@
+const projectCards = {
+  ashford: {
+    bgColor: '#C4375C',
+    header: 'FLOOR JOISTS',
+    imageUrl: 'img/14.-TR.jpg',
+    title: 'PRIVATE HOUSING, 4 PLOTS, ASHFORD',
+    location: 'Ashford, Kent',
+    date: 'September 2020',
+    value: '£21,000'
+  },
+  maidstone: {
+    bgColor: '#8A1537',
+    header: 'TIMBER FRAME',
+    imageUrl: 'maidstone.png',
+    title: 'MAIDSTONE DEVELOPMENT',
+    location: 'Maidstone, Kent',
+    date: 'January 2021',
+    value: '£35,000'
+  }
+};
