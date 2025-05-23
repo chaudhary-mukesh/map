88e9832912f1d1c27877c64f15d84b9c25d64036
chaudhary-mukesh <153888197+chaudhary-mukesh@users.noreply.github.com>
2025-05-23 19:31:28 +0530

    Add files via upload

--- product.js
+++ product.js
@@ -2,7 +2,7 @@ const projectCards = {
   ashford: {
     bgColor: '#C4375C',
     header: 'FLOOR JOISTS',
-    imageUrl: 'img/14.-TR.jpg',
+    imageUrl: 'img/14.EJ_.jpg',
     title: 'PRIVATE HOUSING, 4 PLOTS, ASHFORD',
     location: 'Ashford, Kent',
     date: 'September 2020',
@@ -11,10 +11,360 @@ const projectCards = {
   maidstone: {
     bgColor: '#8A1537',
     header: 'TIMBER FRAME',
-    imageUrl: 'maidstone.png',
+    imageUrl: 'img/general-project_trusses.jpg',
     title: 'MAIDSTONE DEVELOPMENT',
     location: 'Maidstone, Kent',
     date: 'January 2021',
     value: '£35,000'
-  }
+  },
+  eastbourne:{
+     bgColor: '#8a1537',
+    header: 'ROOF TRUSSES',
+    imageUrl: 'img/14.-TR.jpg',
+    title: 'Extension to main house, Eastbourne',
+    location: 'Eastbourne, East Sussex',
+    date: 'May 2017',
+    value: '£10,000'
+  },
+  royalTunbridge:{
+    bgColor: '#c63763',
+    header: 'FLOOR JOISTS',
+    imageUrl: 'img/15.-EJ.jpg',
+    title: 'Private Housing, 2 Plots, Royal Tunbridge Wells',
+    location: 'Royal Tunbridge Wells, Kent',
+    date: 'June 2017',
+    value: '£13,000'
+  },
+   chatham:{
+    bgColor: '#c63763',
+    header: 'FLOOR JOISTS',
+    imageUrl: 'img/7.-Chatham.jpg',
+    title: 'Private Housing, 1 Plot, Chatham',
+    location: 'Chatham, North Kent',
+    date: 'May 2019',
+    value: '£14,000'
+  },
+  westerham:{
+    bgColor: '#8A1537',
+    header: 'ROOF TRUSSES',
+    imageUrl: 'img/general-project_trusses.jpg',
+    title: 'Private Houses, 3 Plots, Westerham',
+    location: 'Westerham, Kent',
+    date: 'August 2017',
+    value: '£100,000'
+  },
+   brentwood:{
+    bgColor: '#8A1537',
+    header: 'ROOF TRUSSES',
+    imageUrl: 'img/general-project_trusses.jpg',
+    title: 'Extension to main house, Brentwood',
+    location: 'Brentwood, Essex',
+    date: 'June 2019',
+    value: '£18,000'
+  },
+    haywards:{
+    bgColor: '#c63763',
+    header: 'ROOF TRUSSES',
+    imageUrl: 'img/general-project_trusses.jpg',
+    title: 'Private Houses, 8 Plots, Haywards Heath',
+    location: 'Haywards Heath, West Sussex',
+    date: 'April 2014',
+    value: '£196,000'
+  },
+    thakeham:{
+    bgColor: '#c63763',
+    header: 'ROOF TRUSSES',
+    imageUrl: 'img/general-project_trusses.jpg',
+    title: 'One-off Bespoke House, Thakeham',
+    location: 'Thakeham, West Sussex',
+    date: 'April 2017',
+    value: '£20,000'
+  },
+    horsham:{
+    bgColor: '#c63763',
+    header: 'ROOF TRUSSES',
+    imageUrl: 'img/10-TR.jpg',
+    title: 'One-off Bespoke House, Horsham',
+    location: 'Horsham, West Sussex',
+    date: 'April 2016',
+    value: '£20,000'
+  },
+    horley:{
+    bgColor: '#402023',
+    header: 'TIMBER FRAMES',
+    imageUrl: 'img/general-project_timber-frames.jpg',
+    title: 'Private Housing 7 Private Houses, Redehall Rd, Horley',
+    location: 'Horley, Surrey',
+    date: 'August 2020',
+    value: '£76,000'
+  },
+    coulsdon:{
+    bgColor: '#402023',
+    header: 'TIMBER FRAMES',
+    imageUrl: 'img/general-project_timber-frames.jpg',
+    title: 'Private Apartments Coulsdon',
+    location: 'Coulsdon, Greater London',
+    date: 'November 2017',
+    value: '£300,000'
+  },
+    byfleet:{
+    bgColor: '#402023',
+    header: 'TIMBER FRAMES',
+    imageUrl: 'img/general-project_timber-frames.jpg',
+    title: 'Social Housing, 5 Terrace Houses, Berry’s Lane, Byfleet',
+    location: 'Byfleet, Surrey',
+    date: 'September 2018',
+    value: '£90,000'
+  },
+    camberley:{
+    bgColor: '#402023',
+    header: 'TIMBER FRAMES',
+    imageUrl: 'img/general-project_timber-frames.jpg',
+    title: 'Private Housing 6 Plots, Deep Cut, Camberley',
+    location: 'Camberley, Surrey',
+    date: 'December 2018',
+    value: '£82,000'
+  },
+  fetcham:{
+    bgColor: '#c63763',
+    header: 'ROOF TRUSSES',
+    imageUrl: 'img/11.-TR.jpg',
+    title: 'Private Houses, 4 Plots, Fetcham',
+    location: 'Fetcham, Surrey',
+    date: 'August 2018',
+    value: '£90,000'
+  },
+   hounslow:{
+    bgColor: '#8A1537',
+    header: 'ROOF TRUSSES',
+    imageUrl: 'img/23.TR_.jpg',
+    title: 'Private Houses, 4 Plots, Hounslow',
+    location: 'Hounslow, Greater London',
+    date: 'July 2017',
+    value: '£107,000'
+  },
+    waltonOnThames:{
+    bgColor: '#8A1537',
+    header: 'ROOF TRUSSES',
+    imageUrl: 'img/general-project_trusses.jpg',
+    title: 'Private Houses, 3 Plots, Walton-on-Thames',
+    location: 'Walton-on-Thames, Surrey',
+    date: 'January 2015',
+    value: '£80,000'
+  },
+     warlingham:{
+    bgColor: '#c63763',
+    header: 'FLOOR JOISTS',
+    imageUrl: 'img/6.Warlingham.jpg',
+    title: 'Private Housing, 8 Plots, Warlingham',
+    location: 'Warlingham, Surrey',
+    date: 'August 2017',
+    value: '£67,000'
+  },
+     ashfords:{
+    bgColor: '#c63763',
+    header: 'FLOOR JOISTS',
+    imageUrl: 'img/12.-Ej.jpg',
+    title: 'Bespoke one off, Flat roof, Ashford',
+    location: 'Ashford, Surrey',
+    date: 'June 2020',
+    value: '£6,500'
+  },
+    dunsfold:{
+    bgColor: '#c63763',
+    header: 'FLOOR JOISTS',
+    imageUrl: 'img/18.-EJ.jpg',
+    title: 'Multiple 10+ Housing site, 42 Plots, Dunsfold',
+    location: 'Dunsfold, Surrey',
+    value: '£53,000'
+  },
+      ealing1:{
+    bgColor: '#402023',
+    header: 'TIMBER FRAMES',
+    imageUrl: 'img/PastedGraphic-43_TR.jpg',
+    title: 'Bespoke One-Off West Lodge Av, Ealing, London',
+    location: 'Ealing, London',
+    date: 'March 2021',
+    value: '£108,000'
+  },
+     ealing2:{
+    bgColor: '#402023',
+    header: 'TIMBER FRAMES',
+    imageUrl: 'img/general-project_timber-frames.jpg',
+    title: 'Private Apartments Cleveland Rd, Ealing, London',
+    location: 'Ealing, London',
+    date: 'March 2020',
+    value: '£110,000'
+  },
+
+      ealing3:{
+    bgColor: '#402023',
+    header: 'TIMBER FRAMES',
+    imageUrl: 'img/2TF.jpg',
+    title: 'Private Apartments Carlton Rd, Ealing, London',
+    location: 'Ealing, London',
+    date: 'March 2021',
+    value: '£201,000'
+  },
+      watford:{
+    bgColor: '#8A1537',
+    header: 'ROOF TRUSSES',
+    imageUrl: 'img/19.-TR.jpg',
+    title: 'Private Houses, 7 plots, Watford',
+    location: 'Watford, Hertfordshire',
+    date: 'May 2015',
+    value: '£168,000'
+  },
+      uxbridge:{
+    bgColor: '#c63763',
+    header: 'FLOOR JOISTS',
+    imageUrl: 'img/general-project_joists.jpg',
+    title: 'Private Housing, 4 plots, Uxbridge',
+    location: 'Uxbridge, Greater London',
+    date: 'February 2020',
+    value: '£33,000'
+  },
+    wandsworth:{
+    bgColor: '#c63763',
+    header: 'FLOOR JOISTS',
+    imageUrl: 'img/general-project_joists.jpg',
+    title: 'Private Apartments, 2 Plots, Wandsworth',
+    location: 'Wandsworth, Greater London',
+    value: '£7,000'
+  },
+    ware:{
+    bgColor: '#402023',
+    header: 'TIMBER FRAMES',
+    imageUrl: 'img/13TF.jpg',
+    title: 'Care Homes 101 Bed Care Home, Ware',
+    location: 'Ware, Hertfordshire',
+    date: 'March 2021',
+    value: '£350,000'
+  },
+
+    stAlbans:{
+    bgColor: '#8A1537',
+    header: 'ROOF TRUSSES',
+    imageUrl: 'img/general-project_trusses.jpg',
+    title: 'Private Houses, 5 plots, St Albans',
+    location: 'St Albans, Hertfordshire',
+    date: 'July 2007',
+    value: '£117,000'
+  },
+    hitchin:{
+    bgColor: '#8A1537',
+    header: 'ROOF TRUSSES',
+    imageUrl: 'img/general-project_trusses.jpg',
+    title: 'Multiple 10+ Housing Site. 12 Plots, Hitchin',
+    location: 'Hitchin, Hertfordshire',
+    date: 'May 2019',
+    value: '£285,000'
+  },
+    luton:{
+    bgColor: '#c63763',
+    header: 'FLOOR JOISTS',
+    imageUrl: 'img/general-project_joists.jpg',
+    title: 'Multiple 10+ Housing site, 41 Plots, Luton',
+    location: 'Luton, Bedfordshire',
+    date: 'June 2019',
+    value: '£59,000'
+  },
+    knebworth:{
+    bgColor: '#c63763',
+    header: 'FLOOR JOISTS',
+    imageUrl: 'img/general-project_joists.jpg',
+    title: 'Bespoke one off, Flat roof, Knebworth',
+    location: 'Knebworth, Hertfordshire',
+    value: '£4,500'
+  },
+    highWycombe:{
+    bgColor: '#8A1537',
+    header: 'ROOF TRUSSES',
+    imageUrl: 'img/21.-TR.jpg',
+    title: 'Private Apartments, High Wycombe',
+    location: 'High Wycombe, Buckinghamshire',
+    date: 'July 2011',
+    value: '£235,000'
+  },
+    aylesbury1:{
+    bgColor: '#c63763',
+    header: 'FLOOR JOISTS',
+    imageUrl: 'img/10.EJ_.jpg',
+    title: 'Multiple 10+ Housing site, 19 Plots, Long Crendon',
+    location: 'Aylesbury, Buckinghamshire',
+    date: 'December 2018',
+    value: '£32,000'
+  },
+      aylesbury2:{
+    bgColor: '#c63763',
+    header: 'FLOOR JOISTS',
+    imageUrl: 'img/11.EJ_.jpg',
+    title: 'Multiple 10+ Housing site, 48 Plots, Aston Clinton',
+    location: 'Aylesbury, Buckinghamshire',
+    date: 'February 2018',
+    value: '£56,000'
+  },
+    goring:{
+    bgColor: '#402023',
+    header: 'TIMBER FRAMES',
+    imageUrl: 'img/general-project_timber-frames.jpg',
+    title: 'Bespoke One-off, Elvendon Rd, Goring',
+    location: 'Goring, Oxfordshire',
+    date: 'February 2017',
+    value: '£34,000'
+  },
+    henleyOnThames:{
+    bgColor: '#402023',
+    header: 'TIMBER FRAMES',
+    imageUrl: 'img/general-project_timber-frames.jpg',
+    title: 'Bespoke One-off, Elvendon Rd, Goring',
+    location: 'Henley-On-Thames, Oxfordshire',
+    date: 'June 2019',
+    value: '£23,000'
+  },
+    didcot:{
+    bgColor: '#8A1537',
+    header: 'ROOF TRUSSES',
+    imageUrl: 'img/1-TR.jpg',
+    title: 'Private Houses, 10 plots, Didcot',
+    location: 'Didcot, Oxfordshire',
+    date: 'July 2017',
+    value: '£277,000'
+  },
+    oxford:{
+    bgColor: '#8A1537',
+    header: 'ROOF TRUSSES',
+    imageUrl: 'img/1-TR.jpg',
+    title: 'Multiple 10+ Housing site, 12 Plots, Oxford',
+    location: 'Oxford, Oxfordshire',
+    date: 'April 2015',
+    value: '£267,000'
+  },
+    upperHeyford:{
+    bgColor: '#c63763',
+    header: 'FLOOR JOISTS',
+    imageUrl: 'img/general-project_joists.jpg',
+    title: 'Multiple 10+ Housing site, 48 Plots, Upper Heyford',
+    location: 'Upper Heyford, Oxfordshire',
+    date: 'January 2017',
+    value: '£94,000'
+  },
+    melksham:{
+    bgColor: '#402023',
+    header: 'TIMBER FRAMES',
+    imageUrl: 'img/general-project_timber-frames.jpg',
+    title: 'Bespoke One-Off, Melksham',
+    location: 'Melksham, Wiltshire',
+    date: 'November 2019',
+    value: '£41,000'
+  },
+   poole:{
+    bgColor: '#c63763',
+    header: 'FLOOR JOISTS',
+    imageUrl: 'img/16.-EJ.jpg',
+    title: 'Private Apartments, 3 Blocks Apartments, Poole',
+    location: 'Poole, Dorset',
+    value: '£147,000'
+  },
 };
+
